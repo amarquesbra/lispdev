2bb39cb59c71dbcd87b9ed98fae740aab584001b
sergey.kolos <sergey.kolos@c960db1b-0f56-0410-bd44-834b428c456c>
2008-09-06 20:58:00 +0000

    SK: Added dummy ReplView.

---
+++ trunk/org.lispdev/src/org/lispdev/views/ReplView.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.lispdev.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.lispdev.LispdevPlugin;
+
+/**
+ * @author sk
+ *
+ */
+public class ReplView extends ViewPart
+{
+  public static final String ID = LispdevPlugin.PLUGIN_ID + ".replView";
+
+  /* (non-Javadoc)
+   * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+   */
+  @Override
+  public void createPartControl(Composite parent)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  /* (non-Javadoc)
+   * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+   */
+  @Override
+  public void setFocus()
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
