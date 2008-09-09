c61955d7c52267bdb9285ddee6a7da974f8ed0ca
sergey.kolos <sergey.kolos@c960db1b-0f56-0410-bd44-834b428c456c>
2008-09-09 00:11:34 +0000

    SK: Extending Repl capabilities - added example Return listener.

--- trunk/org.lispdev/src/org/lispdev/views/repl/IReplInputListener.java
+++ trunk/org.lispdev/src/org/lispdev/views/repl/IReplInputListener.java
@@ -3,11 +3,13 @@
  */
 package org.lispdev.views.repl;
 
+import org.eclipse.swt.events.VerifyEvent;
+
 /**
  * @author sk
  *
  */
 public interface IReplInputListener
 {
-  void run(String msg, String context);
+  void run(String msg, String context,VerifyEvent event);
 }
