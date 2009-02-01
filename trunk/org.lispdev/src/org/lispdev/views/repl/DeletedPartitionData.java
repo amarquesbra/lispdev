2c88c20012a5dd0544819a3781f34c00b3235ae5
sergey.kolos <sergey.kolos@c960db1b-0f56-0410-bd44-834b428c456c>
2009-02-01 13:19:46 +0000

    SK: Incomplete - task 9: Can destroy part of read only using paste http://code.google.com/p/lispdev/issues/detail?id=9 Changed the way the read only partitions are handled when deleted - now can delete several at a time. Also refactored code somewhat.

---
+++ trunk/org.lispdev/src/org/lispdev/views/repl/DeletedPartitionData.java
@@ -0,0 +1,13 @@
+package org.lispdev.views.repl;
+
+import java.util.List;
+
+public class DeletedPartitionData
+{
+  public List<PartitionData> partitions;
+  public String txt;
+  /**
+   * offset from the beginning of edit region
+   */
+  public int offset;
+}
